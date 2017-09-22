05e4ab82e64510e3022cc84ea43a3b24a409dfb9
anton revich <anton@antons-MacBook-Pro.local>
2017-09-21 22:53:17 -0400

    fixed nav

--- index.js
+++ index.js
@@ -1,52 +1,58 @@
 function setBackgroundImage() {
-    $.get(
-        "https://api.unsplash.com/photos/random/?client_id=e823cd6c4a141d7f5c000be889fb7fd4e1887cb05d0746e9f6dbd17a7d412d29", (data) => {
-            $('.hire').css({
-                'background': `url(${data.urls.regular}) no-repeat center`,'background-size': 'cover'
-            });
-            $('.job').css({
-                'background': `url(${data.urls.regular}) no-repeat center`,'background-size': 'cover'
-            });
-        }
-    );
-}
-
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else { 
-        government.HTML = "Geolocation is not supported by this browser.";
+    if($('body').find('#landing').length !== 0){
+        $('body').css('background-image','url("images/gradientbg.jpg")');
+    } else{
+        $.get(
+            "https://api.unsplash.com/photos/random/?client_id=e823cd6c4a141d7f5c000be889fb7fd4e1887cb05d0746e9f6dbd17a7d412d29", (data) => {
+                $('body').css({
+                    'background': `url(${data.urls.regular}`});
+            }
+        );
     }
+    
 }
+
+// function getLocation() {
+//     if (navigator.geolocation) {
+//         navigator.geolocation.getCurrentPosition(showPosition);
+//     } else { 
+//         government.HTML = "Geolocation is not supported by this browser.";
+//     }
+// }
     
     
-function showPosition(position) {
-    var lat, lon;
+// function showPosition(position) {
+//     var lat, lon;
 
-    lat =  position.coords.latitude;
-    lon = position.coords.longitude;
+//     lat =  position.coords.latitude;
+//     lon = position.coords.longitude;
 
-    $.get( "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=Imperial&appid=ed905a0d5167c428dd2bc4f5729121d0", 
-        ( data ) => {
-         $( ".weather" ).append(`<p id='loc'>${data.name}</p>`)
-         $( ".weather" ).append("<span id='weather'>" + data.weather[0].description+ "</span>")
-         $( ".weather" ).append("<span id='temp'>" + data.main.temp+ "</span>")
+//     $.get( "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&units=Imperial&appid=ed905a0d5167c428dd2bc4f5729121d0", 
+//         ( data ) => {
+//          $( ".weather" ).append(`<p id='loc'>${data.name}</p>`)
+//          $( ".weather" ).append("<span id='weather'>" + data.weather[0].description+ "</span>")
+//          $( ".weather" ).append("<span id='temp'>" + data.main.temp+ "</span>")
 
-    });
+//     });
 
-}
+// }
 
 
 
 function modalControll(){
-    $('.nav').click(function(){
-    if($(this).find('.modal-container').attr('id') == 'contact'){
-        $(this).find('.modal-container').addClass('modal-active');
-    } else {
-    $(this).find('.modal-container').toggleClass('modal-active');
-    }
-})
-    }
+    $('.navContainer ul li').click(function(){
+        var listItem = $(this).attr('data-nav');
+        var $findListItem = $('#'+listItem);
+
+        if(!$findListItem.find('.modal-container').hasClass('modal-active')){
+            console.log($findListItem)
+            $('.modal-container').removeClass('modal-active');
+            $findListItem.addClass('modal-active');
+        }else{
+            $findListItem.removeClass('modal-active');
+        }
+    })
+}
 
 
 
@@ -54,9 +60,9 @@ $(document).ready(function() {
     console.log("You are a Developer of some kind arent you!!");
 
 
-    setBackgroundImage();
+    // setBackgroundImage();
 
-    getLocation();
+    // getLocation();
 
     modalControll();
 
